5bee0c8799059e7ee136a32cc3d00c204284ac62
Vivek Kumar <vivekkumar75199@gmail.com>
2025-09-13 18:10:22 +0530

    logout

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -139,13 +139,14 @@ export const login=async (req,res)=>{
     }
     const isMatch=await bcrypt.compare(password,user.password)
     console.log(isMatch);
+
     if(!isMatch){
       return res.status(400).json({
         message:"ivalid email or password"
       })
     }
 
-    jwt.sign({id:user._id,role:user.role},
+   const token= jwt.sign({id:user._id,role:user.role},
       process.env.JWT_SECRET,
       {expiresIn:'24h'}
     );
